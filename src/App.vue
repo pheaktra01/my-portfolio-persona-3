b1b79a6d926ae362c29e06ddd7f35966c8715ec9
Yagami <tranet513@gmail.com>
2026-06-01 22:24:46 +0700

    refactor: replace HelloWorld component with HomeView and remove unused HelloWorld file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/HomeView.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <Home />
 </template>
