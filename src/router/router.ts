686f1a877aa6f2c0fa6e2e98a5932108a7cb969b
Yagami <tranet513@gmail.com>
2026-06-13 18:10:14 +0700

    remove some unuse page and change the router

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,13 +2,10 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 
 // Pages views
 import HomeView from '../views/HomeView.vue'
-import SkillsView from '../views/SkillView.vue'
 import ProjectsView from '../views/ProjectView.vue'
-import StackView from '../views/StackView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import ExperienceView from '../views/ExperienceView.vue'
 import SocialLinksView from '../views/SocialView.vue'
-import CalendarView from '../views/CalendarView.vue'
 import ResumeView from '../views/ResumeView.vue'
 import JourneyView from '../views/JourneyView.vue'
 
@@ -19,19 +16,9 @@ const routes: RouteRecordRaw[] = [
     component: HomeView,
   },
   {
-    path: '/skills',
-    name: 'skills',
-    component: SkillsView,
-  },
-  {
     path: '/projects',
     name: 'projects',
     component: ProjectsView,
-  },
-  {
-    path: '/stack',
-    name: 'stack',
-    component: StackView,
   },
   {
     path: '/profile',
@@ -52,11 +39,6 @@ const routes: RouteRecordRaw[] = [
     path: '/social-links',
     name: 'social-links',
     component: SocialLinksView,
-  },
-  {
-    path: '/calendar',
-    name: 'calendar',
-    component: CalendarView,
   },
   {
     path: '/resume',
